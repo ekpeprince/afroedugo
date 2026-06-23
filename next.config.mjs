8098b90625dd72711b884b6ef29d3a2d8ec62f06
Rokas <jankissyt@gmail.com>
2026-06-23 15:39:55 +0300

    Configure next-pwa to ignore Firebase Auth paths and delete corrupted cache

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const withPWA = withPWAInit({
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  workboxOptions: {
+    navigateFallbackDenylist: [/^\/__\//],
+  }
 });
 
 /** @type {import('next').NextConfig} */
